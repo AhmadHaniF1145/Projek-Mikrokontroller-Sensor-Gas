8d56517f7a6f3fa09027c1676532d58b6e81bd5c
Ilham Kurniawan <ilhamkurniawan1945@gmail.com>
2024-06-03 12:10:42 +0700

    menyempurnakan fungsi ADC pada bare Metal

--- ProgramUtama/Program Utama_Bare Metal_PlatformIO/src/main.cpp
+++ ProgramUtama/Program Utama_Bare Metal_PlatformIO/src/main.cpp
@@ -1,4 +1,4 @@
-//#include <Arduino.h>
+// #include <Arduino.h>
 
 #include <PCMModded.h>
 // Versi modifikasi dari library PCM agar dapat memutar audio
@@ -11,7 +11,7 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 
-//#include <PinChangeInterrupt.h>
+// #include <PinChangeInterrupt.h>
 
 #include "audio1.h"
 #include "audio2.h"
@@ -38,7 +38,8 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 //===========================================================================
 
-void ISR1() {
+void ISR1()
+{
   stopPlayback();
   delay(3000);
 }
@@ -48,31 +49,40 @@ boolean playing(void)
   return TIMSK1 & _BV(OCIE1A);
 }
 
-void putarSuara() {
+void putarSuara()
+{
   loopSound = true;
-  while (loopSound) {
-    
-    if (digitalRead(tombolKanan) == 0) {
+  while (loopSound)
+  {
+
+    if (digitalRead(tombolKanan) == 0)
+    {
       loopSound = false;
       return;
     }
 
-    if (thresholdIndex == 1) {
+    if (thresholdIndex == 1)
+    {
       startPlayback(audio2, sizeof(audio2));
     }
-    else {
+    else
+    {
       startPlayback(audio1, sizeof(audio1));
     }
-    
-    while (playing());
+
+    while (playing())
+      ;
   }
 }
 
-void ThresholdCheck() {
-  if (digitalRead(tombolKiri) == 0) {
+void ThresholdCheck()
+{
+  if (digitalRead(tombolKiri) == 0)
+  {
     delay(500);
     thresholdIndex++;
-    if (thresholdIndex == 3) {
+    if (thresholdIndex == 3)
+    {
       thresholdIndex = 0;
     }
   }
@@ -81,7 +91,8 @@ void ThresholdCheck() {
 
 // ===============================================================================
 
-int main(void) {
+int main(void)
+{
   DDRC |= (1 << redLed);
   DDRL |= (1 << greenLed);
   DDRE &= ~(1 << tombolKanan);
@@ -94,72 +105,54 @@ int main(void) {
   display.display();
 
   // ADC A12
-  
+
   ADCSRA |= (1 << ADPS2) | (1 << ADPS1);
-  ADMUX  |= (1 << ADLAR );
-  ADMUX  |=  _BV(REFS0);
+  ADMUX |= (1 << ADLAR);
+  ADMUX |= _BV(REFS0);
 
   // Mux 5:0 ==>  100100
   ADCSRB |= (1 << MUX5);
   ADMUX |= (1 << MUX2);
 
-  ADCSRA |= (1<< ADEN);
-}
+  ADCSRA |= (1 << ADEN);
 
-// void setup() {
-//   pinMode(redLed, OUTPUT);
-//   pinMode(greenLed, OUTPUT);
-
-//   pinMode(ButtonInt, INPUT);
-//   //attachInterrupt(digitalPinToInterrupt(ButtonInt), ISR1, CHANGE);
-
-//   pinMode(A12, INPUT);
-
-//   pinMode(tombolKanan, INPUT_PULLUP);
-//   pinMode(tombolKiri, INPUT_PULLUP);
-
-//   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-//   display.clearDisplay();
-//   display.display();
-
-//   Serial.begin(9600);
-// }
-
-void loop() {
-  bacaAsap = analogRead(A12);
-  ThresholdCheck();
-
-  Serial.print("Pin A0: ");
-  Serial.println(bacaAsap);
-
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(50, 10);
-  display.print(bacaAsap);
-  display.setCursor(0, 0);
-  display.print("Threshold = ");
-  display.setCursor(70, 0);
-  display.print(sensorThres[thresholdIndex]);
-  display.display();
-
-
-  if (bacaAsap > sensorThres[thresholdIndex])
-  {
-    digitalWrite(redLed, HIGH);
-    digitalWrite(greenLed, LOW);
-
-    putarSuara();
-    //startPlayback(audio1, sizeof(audio1));
-    //while (playing());
+  while (1) {
+     ADCSRA |= (1<<ADSC);
+    while (!(ADCSRA&(1<<ADIF)));
+    ADCSRA |= (1<<ADIF);
+    bacaAsap = ADCH;
     
-  }
-  else
-  {
-    digitalWrite(redLed, LOW);
-    digitalWrite(greenLed, HIGH);
-  }
+    ThresholdCheck();
 
-  delay(300);
+    // Serial.print("Pin A0: ");
+    // Serial.println(bacaAsap);
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(50, 10);
+    display.print(bacaAsap);
+    display.setCursor(0, 0);
+    display.print("Threshold = ");
+    display.setCursor(70, 0);
+    display.print(sensorThres[thresholdIndex]);
+    display.display();
+
+    if (bacaAsap > sensorThres[thresholdIndex])
+    {
+      PORTC |= (1 << redLed);
+      PORTL &= ~(1 << greenLed);
+
+      putarSuara();
+      // startPlayback(audio1, sizeof(audio1));
+      // while (playing());
+    }
+    else
+    {
+      PORTC &= ~(1 << redLed);
+      PORTL |= (1 << greenLed);
+    }
+
+    delay(300);
+  }
 }
-
